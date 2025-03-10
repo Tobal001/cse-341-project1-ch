c0fab4f7f15ad7007d4f1189268efc1507e5d2ec
Cristobal Henriquez <brohenriquez@gmail.com>
2025-03-10 13:36:04 -0400

    Added plain text response instructions

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-// router.get('/', (req, res) => {
-//     res.send('hello world');
-// });
+router.get('/', (req, res) => {
+    res.send('To test Database cluster, go to "/contacts"<br><br>To test individual document go to "/contacts/{_id}" <br>Example _id: 67cca101ac48fd568c391abb');
+    
+});
 
 router.use('/contacts', require('./contacts'));
 
